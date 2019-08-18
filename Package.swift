a7e32daad126a541d26034df329a0fc17e434605
Shawn Clovie <lightpnr@gmail.com>
2019-08-18 16:26:32 +0800

    update dependence Spot 1.0.0

--- Package.swift
+++ Package.swift
@@ -5,16 +5,13 @@ import PackageDescription
 let package = Package(
 	name: "SpotCache",
 	products: [
-		.library(
-			name: "SpotCache",
-			targets: ["SpotCache"]),
+		.library(name: "SpotCache", targets: ["SpotCache"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/shawnclovie/Spot", .branch("master")),
+		.package(url: "https://github.com/shawnclovie/Spot", from: "1.0.0"),
 	],
 	targets: [
-		.target(
-			name: "SpotCache",
-			dependencies: ["Spot"]),
+		.target(name: "SpotCache", dependencies: ["Spot"]),
+		.testTarget(name: "SpotCacheTests", dependencies: ["Spot", "SpotCache"])
 	]
 )
