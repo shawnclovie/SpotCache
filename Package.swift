d43bb883700dced0e7fff8f7c9d4ac8dbfe71c0f
Shawn Clovie <lightpnr@gmail.com>
2019-09-26 11:01:30 +0800

    update SPM file

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
 		.library(name: "SpotCache", targets: ["SpotCache"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/shawnclovie/Spot", from: "1.0.0"),
+		.package(url: "https://github.com/shawnclovie/Spot",
+				 .upToNextMajor(from: "1.0.0")),
 	],
 	targets: [
 		.target(name: "SpotCache", dependencies: ["Spot"]),
